5f6899ffb03009ca91577879403fdad682096b4b
nadavhalahmi <nadavhalahmi@campus.technion.ac.il>
2020-06-19 01:32:19 +0300

    much faster now

--- main.cpp
+++ main.cpp
@@ -1,36 +1,70 @@
 #include <iostream>
 #include <stdlib.h>
 #include <cmath>
+#include <vector>
+
 
 using namespace std;
-typedef unsigned long long ull;
 
-bool prime(ull num) {
-    for(ull i=2; i<=sqrt(num);i++)
-        if(num%i == 0)
+vector<int> set_primes(bool isntPrime[], int size) {
+    vector<int> primes;
+    for(int i=2;i<size;i++){
+        if(!isntPrime[i]){
+            primes.push_back(i);
+            for(int j=2*i; j<size; j+=i){
+                isntPrime[j] = true;
+            }
+        }
+    }
+    return primes;
+}
+
+bool prime(int factor) {
+    for(int i=2; i<=sqrt(factor); i++)
+        if(factor % i == 0)
             return false;
     return true;
 }
 
-ull euler(ull n) {
-    ull res = n;
-    for(ull i=2; i<=n; i++){
-        if(n%i == 0 && prime(i))
-            res = (res/i) * (i-1);
+int euler(int n, vector<int>& primes) {
+    int res = n;
+    for(int i = 0; i<primes.size();i++){
+        int p = primes[i];
+        if(n % p == 0)
+            res = (res/p) * (p-1);
     }
     return res;
 }
 
-int main(int argc, char *argv[]) {
-//    ull n = atoi(argv[1]); //TODO: unnote
-    ull n;
-    cout << "enter number\n";
-    cin >> n;
-    ull res;
-    for(ull i=1; i<=n; i++){
-        res = euler(i);
-//        cout << res << " "; //TODO: unnote
+void update_results(int results[], int size, vector<int>& primes){
+    for(int i=0;i<primes.size();i++){
+        int p = primes[i];
+        results[p] = p-1;
+        for(int j=2*p;j<size;j+=p){
+            results[j] = (results[j]/p) * (p-1);
+        }
     }
-    cout << endl; //TODO: note
+}
+
+int main(int argc, char *argv[]) {
+    int n = atoi(argv[1]); //TODO: unnote
+//    int n;
+//    cout << "enter number\n";
+//    cin >> n;
+    int res;
+    int size = n+1;
+    bool isntPrime[1500000] = {0};
+    int results[size];
+    for(int i=0;i<size;i++){
+        results[i] = i;
+    }
+    vector<int> primes = set_primes(isntPrime, n);
+    update_results(results, size, primes);
+    for(int i=1; i<size; i++){
+        cout << results[i] << " "; //TODO: unnote
+    }
+//    cout << endl; //TODO: note
     return 0;
 }
+
+
