ab8623984007fb50e764627f27a9294113a1304e
nadavhalahmi <nadavhalahmi@campus.technion.ac.il>
2020-11-15 10:12:43 +0200

    updated files with final solution

--- Makefile
+++ Makefile
@@ -1,2 +1,28 @@
-main: main.o
-	g++ main.cpp -o euler
+# This Makefile requires GNU make
+
+CC = g++
+# Add -msse4.2 to enable SSE instrusctions up to SSE4.2.
+# We use optimization level 1 to avoid automatic vectorization with both GCC
+# and Clang. GCC versions 4 and 5 will do automatic vectorization only at
+# optimization level 3. Clang apparently will do it at level 2 or 3.
+CFLAGS =  -O3  -msse4.2 -mavx -march=native
+ASFLAGS = -g
+
+
+DIST_FILES =  \
+    main.cpp \
+    Makefile
+
+all: main
+
+main: main.cpp
+	$(CC) $(CFLAGS) -fprofile-generate=. -o euler $^
+	./euler 1500000 > /dev/null
+	$(CC) $(CFLAGS) -fprofile-use=. -o euler $^
+
+
+
+
+
+clean:
+	rm -f *.o
--- main.cpp
+++ main.cpp
@@ -2,16 +2,18 @@
 #include <stdlib.h>
 #include <cmath>
 #include <vector>
-
+#include <immintrin.h>
+#include <nmmintrin.h>
+#include <stdio.h>
 
 using namespace std;
 
-vector<int> set_primes(bool isntPrime[], int size) {
+inline vector<int> set_primes(vector<bool>& isntPrime) {
     vector<int> primes;
-    for(int i=2;i<size;i++){
+    for(int i=2;i<isntPrime.size();i++){
         if(!isntPrime[i]){
             primes.push_back(i);
-            for(int j=2*i; j<size; j+=i){
+            for(int j=2*i; j<isntPrime.size(); j+=i){
                 isntPrime[j] = true;
             }
         }
@@ -19,51 +21,69 @@ vector<int> set_primes(bool isntPrime[], int size) {
     return primes;
 }
 
-bool prime(int factor) {
-    for(int i=2; i<=sqrt(factor); i++)
-        if(factor % i == 0)
-            return false;
-    return true;
-}
-
-int euler(int n, vector<int>& primes) {
-    int res = n;
-    for(int i = 0; i<primes.size();i++){
-        int p = primes[i];
-        if(n % p == 0)
-            res = (res/p) * (p-1);
-    }
-    return res;
-}
-
-void update_results(int results[], int size, vector<int>& primes){
+inline void update_results(int results[], int size, vector<int>& primes){
     for(int i=0;i<primes.size();i++){
         int p = primes[i];
         results[p] = p-1;
         for(int j=2*p;j<size;j+=p){
+            __builtin_prefetch (&results[j+3*p], 1, 0);
             results[j] = (results[j]/p) * (p-1);
         }
     }
 }
 
+ #define pc(x) putchar_unlocked(x);
+    inline void writeInt (int N)
+    {
+        pc(' ');
+        (N >= 1000000) && pc(N/1000000 + '0');
+        (N >= 100000) && pc(N/100000%10 + '0');
+        (N >= 10000) && pc(N/10000%10 + '0');
+        (N >= 1000) && pc(N/1000%10 + '0');
+        (N >= 100) && pc(N/100%10 + '0');
+        (N >= 10) && pc(N/10%10 + '0');
+        pc(N%10 + '0');   
+    }
+    
+    
+
 int main(int argc, char *argv[]) {
-    int n = atoi(argv[1]); //TODO: unnote
-//    int n;
-//    cout << "enter number\n";
-//    cin >> n;
-    int res;
+    int n = atoi(argv[1]);
     int size = n+1;
-    bool isntPrime[1500000] = {0};
+    vector<bool> isntPrime(size, false);
     int results[size];
-    for(int i=0;i<size;i++){
-        results[i] = i;
+    vector<int> primes;
+    int i;
+    for(i=0;i<=size-8;i+=8){
+        __m256i values = _mm256_setr_epi32(i, i+1, i+2, i+3, i+4, i+5, i+6, i+7);
+        _mm256_storeu_si256((__m256i*) &results[i], values);
+        //results[i] = i;
+        //results[i+1] = i+1;
+        //results[i+2] = i+2;
+        //results[i+3] = i+3;
+        //results[i+4] = i+4;
+        //results[i+5] = i+5;
+        //results[i+6] = i+6;
+        //results[i+7] = i+7;
     }
-    vector<int> primes = set_primes(isntPrime, n);
+    for(;i<size;i++)
+      results[i] = i;
+    primes = set_primes(isntPrime);
     update_results(results, size, primes);
-    for(int i=1; i<size; i++){
-        cout << results[i] << " "; //TODO: unnote
+    pc('1');
+    for(i=2; i<=size-8; i+=8){
+        //__builtin_prefetch (&results[i+16], 0, 0);
+        writeInt(results[i]);
+        writeInt(results[i+1]);
+        writeInt(results[i+2]);
+        writeInt(results[i+3]);
+        writeInt(results[i+4]);
+        writeInt(results[i+5]);
+        writeInt(results[i+6]);
+        writeInt(results[i+7]);
     }
-//    cout << endl; //TODO: note
+    for(;i<size;i++)
+      writeInt(results[i]);
     return 0;
 }
 
